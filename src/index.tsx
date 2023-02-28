a1f14ab238190714bb6cd68d981cb7d979a6f5a3
ko-inseoklee <tjrkd222@gmail.com>
2023-02-28 11:20:43 +0900

    각 예제 설명 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './vac-example/Counter';
+import Counter from './vac/Counter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
